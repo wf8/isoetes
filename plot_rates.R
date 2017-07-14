919a83bdc0ea36ead5f2e8e0b8b626fa1f693dd1
Will Freyman <willfreyman@gmail.com>
2017-07-14 01:36:16 -0700

    improving plot

--- plot_rates.R
+++ plot_rates.R
@@ -9,9 +9,10 @@ data = data.frame(rate=c(log$rate_01, log$rate_10), type=rep(c("Trilobate to bil
 p = ggplot(data, aes(x=rate, fill=type)) + 
           labs(x="Rate", y="Posterior Density") + 
           geom_density(alpha = 0.4) + 
+          scale_fill_manual(values = c(hue_pal()(3)[2],hue_pal()(3)[3])) +
           guides(fill = guide_legend(title = "Transition Rates")) +
-          geom_vline(xintercept = mean(log$rate_01), linetype = "longdash", color=hue_pal()(2)[2]) +
-          geom_vline(xintercept = mean(log$rate_10), linetype = "longdash", color=hue_pal()(2)[1]) 
+          geom_vline(xintercept = mean(log$rate_01), linetype = "longdash", color=hue_pal()(3)[3]) +
+          geom_vline(xintercept = mean(log$rate_10), linetype = "longdash", color=hue_pal()(3)[2]) 
 
 print(p)
 
